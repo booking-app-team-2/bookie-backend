545e241f1f9021c68a31db7bc8cfadade66f85d2
darkokos <darkosvilar02@gmail.com>
2023-11-08 21:53:48 +0100

    Created spring boot project.

---
+++ src/main/java/booking_app_team_2/bookie/BookieApplication.java
@@ -0,0 +1,13 @@
+package booking_app_team_2.bookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/booking_app_team_2/bookie/BookieApplicationTests.java
@@ -0,0 +1,13 @@
+package booking_app_team_2.bookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
