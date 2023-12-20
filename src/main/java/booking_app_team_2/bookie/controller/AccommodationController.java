3e8ed9479f484a7f1b404103c21da511d94ee4bf
darkokos <darkosvilar02@gmail.com>
2023-12-20 21:29:17 +0100

    fix: removed duplicate code

--- src/main/java/booking_app_team_2/bookie/controller/AccommodationController.java
+++ src/main/java/booking_app_team_2/bookie/controller/AccommodationController.java
@@ -57,17 +57,6 @@ public class AccommodationController {
         return new ResponseEntity<>(accommodations, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/unapproved", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<AccommodationDTO>> getUnapprovedAccommodations() {
-        Collection<AccommodationDTO> accommodations = accommodationService
-                .findAllByIsApproved(false)
-                .stream()
-                .map(accommodation -> new AccommodationDTO())
-                .toList();
-
-        return new ResponseEntity<>(accommodations, HttpStatus.OK);
-    }
-
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AccommodationDTO> getAccommodation(@PathVariable Long id) {
         Optional<Accommodation> accommodation = accommodationService.findOne(id);
