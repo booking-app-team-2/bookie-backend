426abe3c48a1f6fafe866de6d180db1e47848fdf
darkokos <darkosvilar02@gmail.com>
2024-01-18 02:02:22 +0100

    feat: user profile-changing data validation

--- src/main/java/booking_app_team_2/bookie/dto/UserBasicInfoDTO.java
+++ src/main/java/booking_app_team_2/bookie/dto/UserBasicInfoDTO.java
@@ -1,9 +1,12 @@
 package booking_app_team_2.bookie.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserBasicInfoDTO {
+    @NotBlank(message = "Name must not be blank.")
     private String name;
+    @NotBlank(message = "Surname must not be blank.")
     private String surname;
 }
